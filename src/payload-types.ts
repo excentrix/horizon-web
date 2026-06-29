74c1c023d366e0b8d5ed3c4c4cbd3c0b359f0c85
SidKarthik <sa268592@gmail.com>
2026-06-29 21:38:00 +0530

    feat: enable API key auth on Users collection for programmatic blog post uploads

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -434,6 +434,9 @@ export interface User {
   name?: string | null;
   updatedAt: string;
   createdAt: string;
+  enableAPIKey?: boolean | null;
+  apiKey?: string | null;
+  apiKeyIndex?: string | null;
   email: string;
   resetPasswordToken?: string | null;
   resetPasswordExpiration?: string | null;
@@ -1598,6 +1601,9 @@ export interface UsersSelect<T extends boolean = true> {
   name?: T;
   updatedAt?: T;
   createdAt?: T;
+  enableAPIKey?: T;
+  apiKey?: T;
+  apiKeyIndex?: T;
   email?: T;
   resetPasswordToken?: T;
   resetPasswordExpiration?: T;
